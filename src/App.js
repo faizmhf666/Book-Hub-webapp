849e51c4b246cc70a65ad4d23a195782c37a4ad8
faizmhf666 <faizmhf666@gmail.com>
2023-05-25 14:18:43 +0000

    All Cases Cleared, working on CSS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
+import {Switch, Route, Redirect} from 'react-router-dom'
 import Login from './components/Login'
 import Home from './components/Home'
 import Bookshelves from './components/Bookshelves'
@@ -9,7 +9,7 @@ import NotFound from './components/NotFound'
 import './App.css'
 
 const App = () => (
-  <BrowserRouter>
+  <>
     <Switch>
       <Route exact path="/login" component={Login} />
       <ProtectedRoute exact path="/" component={Home} />
@@ -18,8 +18,7 @@ const App = () => (
       <Route path="/not-found" component={NotFound} />
       <Redirect to="not-found" />
     </Switch>
-  </BrowserRouter>
+  </>
 )
 
 export default App
-/*       */
